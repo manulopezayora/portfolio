935775c9802f1d233415df56bebc7ab9573c53be
Manu Lopez <manulopezayora1985@protonmail.com>
2021-04-27 19:03:48 +0200

    [DEV] Created components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    WorksComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
