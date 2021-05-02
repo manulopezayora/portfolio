b42c0cddb586d79e8c241d99a9d586cc2d4733ad
Manu Lopez <manulopezayora1985@protonmail.com>
2021-05-02 19:43:15 +0200

    [WIP]

--- src/app/pages/works/works.component.ts
+++ src/app/pages/works/works.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorksComponent implements OnInit {
 
+  worksList = [
+    {
+      image:'../../../assets/img/Maps.png',
+      title: 'Maps App',
+      subtitle: 'HTML - CSS - JavaScript',
+      tecnologies: 'vanila',
+      explanation: 'Proyecto de marcador de mapas con la API de Leaflet. Las coordenadas son las que da google en la URL de google maps.'
+    },
+    {
+      image:'../../../assets/img/Maps.png',
+      title: 'Maps App',
+      subtitle: 'HTML - CSS - JavaScript',
+      tecnologies: 'vanila',
+      explanation: 'Proyecto de marcador de mapas con la API de Leaflet. Las coordenadas son las que da google en la URL de google maps.'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
